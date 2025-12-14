6ad6c3455d5392195ea5d0ed885e76994491cb97
Bruno Aravena Maguida <bruam23@gmail.com>
2025-12-13 22:49:44 -0600

    Update merge_all_data.R

--- code/merge_all_data.R
+++ code/merge_all_data.R
@@ -2,8 +2,8 @@
 data_wd        <-  "C:/Users/xd-br/Dropbox/causal_schools"
 code_output_wd <-  "C:/Users/xd-br/Desktop/PhD/Research/causal_schools"
 
-#data_wd <- "C:/Users/brunem/Dropbox/causal_schools"
-#code_output_wd <-  "C:/Users/brunem/Research/causal_schools"
+data_wd <- "C:/Users/brunem/Dropbox/causal_schools"
+code_output_wd <-  "C:/Users/brunem/Research/causal_schools"
 
 #Datawd (Dropbox) 
 setwd(data_wd)
